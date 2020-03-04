f6eaa7f0cc52df27c298d89462539284357b0167
Trevor Mackin <tismetrm@gmail.com>
2020-03-04 09:14:15 -0800

    Add dependencies to project

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Help Queue',
+      title: 'React API',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
